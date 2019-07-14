253bc102393b78cd73b9eb8a91f1fd91b6317738
Emilio Platzer <emilioplatzer@gmail.com>
2019-07-14 01:19:00 -0300

    Mejoro el informe de tipos

--- like-ar.d.ts
+++ like-ar.d.ts
@@ -1,19 +1,19 @@
 declare module "like-ar"{
-    type ObjectWithArrayFunctions<T>={
-        forEach:( callback:(value:T  , key:string)=>void   ) => ObjectWithArrayFunctions<T>
-        map    :<U>( callback:(value:T, key:string)=>U     ) => ObjectWithArrayFunctions<U>
-        filter :( callback:(value:T  , key:string)=>boolean) => ObjectWithArrayFunctions<T>
-        build  :<U>( callback:(value:T, key:string)=>{[key:string]:U}) => ObjectWithArrayFunctions<U>
-        keys   :() => string[]
+    type ObjectWithArrayFunctions<T, K extends keyof any>={
+        forEach:   ( callback:(value:T, key:K)=>void   ) => ObjectWithArrayFunctions<T,K>
+        map    :<U>( callback:(value:T, key:K)=>U      ) => ObjectWithArrayFunctions<U,K>
+        filter :   ( callback:(value:T, key:K)=>boolean) => ObjectWithArrayFunctions<T,K>
+        build  :<U,K2 extends keyof {}>(callback:(value:T,key:K)=>{[key in K2]:U}) => ObjectWithArrayFunctions<U,K2>
+        keys   :() => K[]
         array  :() => T[]
-        plain  :() => {[key:string]:T}
+        plain  :() => {[key in K]:T}
         join   :(separator:string) => string
     }
-    function likeAr<T>(o:{[key:string]:T}):ObjectWithArrayFunctions<T>
+    function likeAr<T, K extends keyof any>(o:{[key in K]: T}):ObjectWithArrayFunctions<T, K>
     namespace likeAr{
         function toPlainObject<T>(array:[string, T][]):{[key:string]:T}
         function toPlainObject<T, K extends keyof T>(array:T, keyName:K, valueName:K):{[key:string]:T}
-        function toPlainObject<T>(arrayKeys:[string], arrayValues:[T]):{[key:string]:T}
+        function toPlainObject<T, K extends keyof any>(arrayKeys:K[], arrayValues:T[]):{[key in K]:T}
     }
     export = likeAr
 }
