f7e7bfbcb3cb6d1669e04f389e0110148a7d2795
Emilio Platzer <emilioplatzer@gmail.com>
2019-09-21 10:25:28 -0300

    Pongo optativas los parámetros de los callbacks

--- like-ar.d.ts
+++ like-ar.d.ts
@@ -1,15 +1,16 @@
 declare module "like-ar"{
     type ObjectWithArrayFunctions<T, K extends keyof any>={
-        forEach:   ( callback:(value:T, key:K, original:{[key in K]:T}, pos:number)=>void   ) => ObjectWithArrayFunctions<T,K>
-        map    :<U>( callback:(value:T, key:K, original:{[key in K]:T}, pos:number)=>U      ) => ObjectWithArrayFunctions<U,K>
-        filter :   ( callback:(value:T, key:K, original:{[key in K]:T}, pos:number)=>boolean) => ObjectWithArrayFunctions<T,K>
-        build  :<U,K2 extends keyof {}>(callback:(value:T,key:K,original:{[key in K]:T},pos:number)=>{[key in K2]:U}) => ObjectWithArrayFunctions<U,K2>
+        forEach:   ( callback:(value:T, key?:K, original?:{[key in K]:T}, pos?:number)=>void   ) => ObjectWithArrayFunctions<T,K>
+        map    :<U>( callback:(value:T, key?:K, original?:{[key in K]:T}, pos?:number)=>U      ) => ObjectWithArrayFunctions<U,K>
+        filter :   ( callback:(value:T, key?:K, original?:{[key in K]:T}, pos?:number)=>boolean) => ObjectWithArrayFunctions<T,K>
+        build  :<U,K2 extends keyof {}>(callback:(value:T,key?:K,original?:{[key in K]:T},pos?:number)=>{[key in K2]:U}) => ObjectWithArrayFunctions<U,K2>
         keys   :() => K[]
         array  :() => T[]
         plain  :() => {[key in K]:T}
         join   :(separator:string) => string
         keyCount:()=>number        
     }
+    function likeAr<T, K extends keyof any>(a:T[]):ObjectWithArrayFunctions<T, K>
     function likeAr<T, K extends keyof any>(o:{[key in K]: T}):ObjectWithArrayFunctions<T, K>
     namespace likeAr{
         function toPlainObject<T>(array:[string, T][]):{[key:string]:T}
